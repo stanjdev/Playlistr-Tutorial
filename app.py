e5832965ed295ffea8b94deb3954dfb6566d9151
Stanley <stanley.jeong@shopify.com>
2021-10-22 17:20:04 -0700

    Users can see all playlists

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#   return render_template('home.html', msg='Flask is cool!!')
+
+playlists = [
+  { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+  { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
+  { 'title': 'MC Hammer', 'description': 'Hammer Time!' }
+]
+
 @app.route('/')
-def index():
-  return render_template('home.html', msg='Flask is cool!!')
+def playlists_index():
+  """ Show all playlists """
+  return render_template('playlists_index.html', playlists=playlists)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
-
 
