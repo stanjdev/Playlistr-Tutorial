cd5e883887b9e24c78e9f974f38cbf7f6010ef62
Stanley <stanley.jeong@shopify.com>
2021-11-01 20:50:44 -0700

    Users can see comments

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ db = client.get_default_database()
 # db = client.Playlister
 # one collection from the database called 'playlists'
 playlists = db.playlists
+comments = db.comments
 
 app = Flask(__name__)
 
@@ -67,7 +68,8 @@ def playlists_submit():
 def playlists_show(playlist_id):
   """ Show a single playlist """
   playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-  return render_template('playlists_show.html', playlist=playlist)
+  playlist_comments = comments.find_one({'playlist_id': ObjectId(playlist_id)})
+  return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
@@ -99,6 +101,17 @@ def playlists_delete(playlist_id):
   """ Delete one playlist """
   playlists.delete_one({'_id': ObjectId(playlist_id)})
   return redirect(url_for('playlists_index'))
+
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+  """ Submit a new comment """
+  comment = {
+    'title': request.form.get('title'),
+    'content': request.form.get('content'),
+  }
+  comments.insert_one(comment)
+  return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
 
 
