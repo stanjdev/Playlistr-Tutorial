5fb5c6a5754d2dca79f3b39f59ada943fdbf69eb
Stanley <stanley.jeong@shopify.com>
2021-11-01 22:36:53 -0700

    fixed delete button

--- app.py
+++ app.py
@@ -37,16 +37,19 @@ def video_url_creator(id_list):
 #   { 'title': 'MC Hammer', 'description': 'Hammer Time!' }
 # ]
 
+
 @app.route('/')
 def playlists_index():
   """ Show all playlists """
   return render_template('playlists_index.html', playlists=playlists.find())
+
 
 @app.route('/playlists/new')
 def playlists_new():
   """ Create a new playlist """
   playlist = []
   return render_template('playlists_new.html', playlist=playlist, title="New Playlist")
+
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -64,18 +67,21 @@ def playlists_submit():
   playlists.insert_one(playlist)
   return redirect(url_for('playlists_index'))
 
+
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
   """ Show a single playlist """
   playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-  playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
+  playlist_comments = comments.find({'playlist_id': playlist_id})
   return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
+
 
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
   """ Show the edit form for the playlist """
   playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
   return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -96,29 +102,35 @@ def playlists_update(playlist_id):
   # take us ack to the playlist's show page
   return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+
 @app.route('/playlists/<playlist_id>/delete', methods=['POST'])
 def playlists_delete(playlist_id):
   """ Delete one playlist """
   playlists.delete_one({'_id': ObjectId(playlist_id)})
   return redirect(url_for('playlists_index'))
 
+
 @app.route('/playlists/comments', methods=['POST'])
 def comments_new():
   """ Submit a new comment """
   comment = {
-    'playlist_id': ObjectId(request.form.get('playlist_id')),
+    'playlist_id': request.form.get('playlist_id'),
     'title': request.form.get('title'),
     'content': request.form.get('content'),
-    'comment_id': ObjectId()
   }
   comments.insert_one(comment)
   return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
-# @app.route('/playlists/comments/<comment_id>', methods=['POST'])
-# def delete_comment(comment_id):
-#   """ Delete a comment """
-#   comments.delete_one({'comment_id': ObjectId(comment_id)})
-#   return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+
+# USING AN <a> TAG WITHOUT POST METHOD WORKS
+@app.route('/playlists/<playlist_id>/comments/<comment_id>/delete')
+def delete_comment(playlist_id, comment_id):
+  """ Delete a comment """
+  comments.delete_one({'_id': ObjectId(comment_id)})
+  return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
